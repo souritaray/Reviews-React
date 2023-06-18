6006c0cc479a59829fd205ac31ce796f0251c823
souritaray <100030958+souritaray@users.noreply.github.com>
2023-06-18 06:52:40 +0000

    added review component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
-import './App.css';
-
+import Reviews from "./Reviews";
 function App() {
-
+return(
+    <main>
+        <section className="container">
+            <article className="title">
+                <h2>Our Reviews</h2>
+                <div class="underline"></div>
+            </article>
+            <Reviews />
+        </section>
+    </main>
+)
 }
 
 export default App;
