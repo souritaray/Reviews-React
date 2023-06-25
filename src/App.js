02fb4b986ed1f49458877bb9f804e13a6088ce67
souritaray <100030958+souritaray@users.noreply.github.com>
2023-06-25 14:06:51 +0000

    refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import Reviews from "./Reviews";
+import reviews from "./data";
 function App() {
 return(
     <main>
         <section className="container">
             <article className="title">
                 <h2>Our Reviews</h2>
-                <div class="underline"></div>
+                <div className="underline"></div>
             </article>
-            <Reviews />
+            <Reviews reviews={reviews}/>
         </section>
     </main>
 )
